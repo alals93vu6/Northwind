ebd308e5c6f5158bca59408b9641bbf7e1536beb
alals93vu6 <a0987915131@gmail.com>
2025-09-06 18:45:14 +0800

    New Project setting

---
+++ NorthwindTest/NorthwindTest/Controllers/NorthwindController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+namespace NorthwindTest.Controllers
+{
+    [ApiController]
+    [Route("customer/[controller]")]
+    public class NorthwindController
+    {
+
+        public readonly IMediator _mediator;
+        public NorthwindController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+    }
+}
