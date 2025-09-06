37983f91c1a479435aeec699a19a963dcf2bce29
alals93vu6 <a0987915131@gmail.com>
2025-09-06 19:39:52 +0800

    Added SQL Server Setting

---
+++ NorthwindTest/NorthwindTest/Data/NorthwindContext.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindTest.Modles;
+
+namespace NorthwindTest.Data;
+
+public class NorthwindContext : DbContext
+{
+    public NorthwindContext(DbContextOptions<NorthwindContext> options)
+        : base(options)
+    {
+    }
+    
+    public DbSet<CustomerItem> Customers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CustomerItem>(entity =>
+        {
+            entity.ToTable("Customers");
+
+            // 主鍵
+            entity.HasKey(e => e.CustomerID);
+
+            // 欄位對映
+            entity.Property(e => e.CustomerID)
+                  .HasColumnName("CustomerID")
+                  .HasMaxLength(5)
+                  .IsRequired(); // Northwind 的主鍵是 nvarchar(5)
+
+            entity.Property(e => e.CompanyName)
+                  .HasMaxLength(40);
+
+            entity.Property(e => e.ContactName)
+                  .HasMaxLength(30);
+
+            entity.Property(e => e.ContactTitle)
+                  .HasMaxLength(30);
+
+            entity.Property(e => e.Address)
+                  .HasMaxLength(60);
+
+            entity.Property(e => e.City)
+                  .HasMaxLength(15);
+
+            entity.Property(e => e.Region)
+                  .HasMaxLength(15);
+
+            entity.Property(e => e.PostalCode)
+                  .HasMaxLength(10);
+
+            entity.Property(e => e.Country)
+                  .HasMaxLength(15);
+
+            entity.Property(e => e.Phone)
+                  .HasMaxLength(24);
+
+            entity.Property(e => e.Fax)
+                  .HasMaxLength(24);
+        });
+    }
+
+}
---
+++ NorthwindTest/NorthwindTest/Modles/CustomerItem.cs
@@ -0,0 +1,17 @@
+﻿namespace NorthwindTest.Modles
+{
+    public class CustomerItem
+    {
+        public int CustomerID { get; set; } //客戶ID
+        public string? CompanyName { get; set; } //所在公司名稱
+        public string? ContactName { get; set; } //客戶姓名
+        public string? ContactTitle { get; set; } //客戶頭銜
+        public string? Address { get; set; } //聯絡地址
+        public string? City { get; set; } //所在城市
+        public string? Region { get; set; } //所在地區
+        public string? PostalCode { get; set; } //郵編
+        public string? Country { get; set; } //國家
+        public string? Phone { get; set; } //電話
+        public string? Fax { get; set; } //傳真
+    }
+}
